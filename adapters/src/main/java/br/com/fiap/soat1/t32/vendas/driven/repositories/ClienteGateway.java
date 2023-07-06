2e5e3ef9d5490690a91e5ecd305b4b247acbcdee
silvester_silva <silvester_silva@hotmail.com>
2023-07-05 21:30:46 -0300

    ajuste exceção duplicate key

--- adapters/src/main/java/br/com/fiap/soat1/t32/vendas/driven/repositories/ClienteGateway.java
+++ adapters/src/main/java/br/com/fiap/soat1/t32/vendas/driven/repositories/ClienteGateway.java
@@ -1,11 +1,14 @@
 package br.com.fiap.soat1.t32.vendas.driven.repositories;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import br.com.fiap.soat1.t32.vendas.domain.Cliente;
 import br.com.fiap.soat1.t32.vendas.driven.repositories.dao.ClienteDao;
 import br.com.fiap.soat1.t32.vendas.ports.ClientePort;
 import br.com.fiap.soat1.t32.vendas.utils.mappers.ClienteMapper;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,7 +19,11 @@ public class ClienteGateway implements ClientePort {
 
 	@Override
 	public void cadastrarCliente(Cliente cliente) {
+		try {
 		clienteDao.save(ClienteMapper.toEntity(cliente));
+		} catch (DataIntegrityViolationException e) {
+			throw new DuplicateKeyException("CPF informado já cadastrado!");
+		}
 	}
 
 	@Override
